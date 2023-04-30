d742a01bffc6a5c3b0895eda103fbe8b0893f0a1
RVR0MAN <120349770+RVR0MAN@users.noreply.github.com>
2023-04-30 21:40:28 +0300

    Update SpaceContent

--- src/main/java/SpaceContent.java
+++ src/main/java/SpaceContent.java
@@ -6,19 +6,20 @@ public class SpaceContent {
     private final String date;
     private final String explanation;
     private final String hdurl;
-    private final String media_type;
-    private final String service_version;
+    private final String mediaType;
+    private final String serviceVersion;
     private final String title;
     private final String url;
 
-
+//аннотация JsonProperty служит для "мэпинга" полей из json с полями класса, поэтому поля класса
+    //требуется писать соблюдая camelCase
     public SpaceContent(
             @JsonProperty("copyright")String copyright,
             @JsonProperty("date")String date,
             @JsonProperty("explanation")String explanation,
             @JsonProperty("hdurl")String hdurl,
-            @JsonProperty("media_type")String media_type,
-            @JsonProperty("service_version")String service_version,
+            @JsonProperty("media_type")String mediaType,
+            @JsonProperty("service_version")String serviceVersion,
             @JsonProperty("title")String title,
             @JsonProperty("url")String url
     ){
@@ -26,8 +27,8 @@ public class SpaceContent {
         this.date=date;
         this.explanation=explanation;
         this.hdurl=hdurl;
-        this.media_type=media_type;
-        this.service_version=service_version;
+        this.mediaType=mediaType;
+        this.serviceVersion=serviceVersion;
         this.title=title;
         this.url=url;
     }
@@ -43,8 +44,8 @@ public class SpaceContent {
                 "\n date=" + date +
                 "\n explanation=" + explanation +
                 "\n hdurl=" + hdurl +
-                "\n media_type=" + media_type +
-                "\n service_version=" + service_version +
+                "\n media_type=" + mediaType +
+                "\n service_version=" + serviceVersion +
                 "\n title=" + title +
                 "\n url=" + url;
     }
