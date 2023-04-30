a8674a15718a7ab98f4f39fa963ba23e2b713de3
RVR0MAN <120349770+RVR0MAN@users.noreply.github.com>
2023-04-30 21:39:00 +0300

    Update Main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -14,7 +14,7 @@ import java.io.InputStream;
 import java.net.URI;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.List;
+
 
 public class Main {
     public static ObjectMapper mapper = new ObjectMapper();
@@ -34,12 +34,22 @@ public class Main {
 
         CloseableHttpResponse response = httpClient.execute(request);
 
+        /*если в ответ на запрос мы должны получить не массив объектов, а один объект,
+        есть несколько вариантов решения:
+
+        1.При создании экземпляра spaceContent, тип указываем SpaceContent,
+        а не List<SpaceContent>, также при создании экземпляра TypeReference указываем тип <SpaceContent>
+
+        2.Если же указали List, то используем настройку для mapper,
+        которая оборачивает один объект в ArrayList, с одним значением:
+        
         mapper.configure(DeserializationFeature.ACCEPT_SINGLE_VALUE_AS_ARRAY, true);
+         */
 
-       List<SpaceContent> spaceContent = mapper.readValue(
-                response.getEntity().getContent(), new TypeReference<List<SpaceContent>>(){});
+        SpaceContent spaceContent = mapper.readValue(
+                response.getEntity().getContent(), new TypeReference<SpaceContent>(){});
 
-       download(spaceContent.get(spaceContent.size()-1).getUrl(), "image.jpg");
+       download(spaceContent.getUrl(), "image.jpg");
         response.close();
         httpClient.close();
     }
